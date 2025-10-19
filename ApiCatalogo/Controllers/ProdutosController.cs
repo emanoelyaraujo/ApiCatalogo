b015ee649d22c562209ffbb784af747b9b6fcf96
Emanoely Araújo <emanoelyaraujo004@gmail.com>
2025-10-19 13:57:53 -0300

    ajustar crud produtos e criar crud categorias

--- ApiCatalogo/Controllers/ProdutosController.cs
+++ ApiCatalogo/Controllers/ProdutosController.cs
@@ -20,67 +20,102 @@ namespace ApiCatalogo.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<Produto>> Get()
         {
-            var produtos = _context.Produtos.ToList();
-
-            if(produtos is null)
+            try
             {
-                return NotFound("Produtos não encontrados.");
-            }
+                var produtos = _context.Produtos.ToList();
 
-            return produtos;
+                if (produtos is null)
+                {
+                    return NotFound("Produtos não encontrados.");
+                }
+
+                return produtos;
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um erro ao tratar sua solicitação.");
+            }
         }
 
         [HttpGet("{id:int}", Name="ObterProduto")]
         public ActionResult<Produto> Get(int id)
         {
-            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
-            if (produto is null)
+            try
             {
-                return NotFound("Produto não encontrado."); 
+                var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+                if (produto is null)
+                {
+                    return NotFound($"Produto com id = {id} não encontrado.");
+                }
+                return produto;
             }
-            return produto;
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um erro ao tratar sua solicitação.");
+            }
         }
 
         [HttpPost]
         public ActionResult Post(Produto produto)
         {
-            if(produto is null)
+            try
             {
-                return BadRequest("Envie os campos do produto no payload da requisição.");
-            }
+                if (produto is null)
+                {
+                    return BadRequest("Envie os campos do produto no payload da requisição.");
+                }
 
-            _context.Produtos.Add(produto);
-            _context.SaveChanges();
-            return new CreatedAtRouteResult("ObterProduto", new {id = produto.ProdutoId}, produto);
+                _context.Produtos.Add(produto);
+                _context.SaveChanges();
+                return new CreatedAtRouteResult("ObterProduto", new { id = produto.ProdutoId }, produto);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um erro ao tratar sua solicitação.");
+            }
         }
 
         [HttpPut("{id:int}")]
         public ActionResult Put(int id, Produto produto) {
-            if (id != produto.ProdutoId)
+            try
             {
-                return BadRequest("O ID do produto na URL é diferente do ID do produto no payload. Verifique e ajuste os valores");
+                if (id != produto.ProdutoId)
+                {
+                    return BadRequest("O ID do produto na URL é diferente do ID do produto no payload. Verifique e ajuste os valores");
+                }
+
+                _context.Entry(produto).State = EntityState.Modified;
+                _context.SaveChanges();
+
+                return Ok(produto);
             }
-
-            _context.Entry(produto).State = EntityState.Modified;
-            _context.SaveChanges();
-
-            return Ok(produto);
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um erro ao tratar sua solicitação.");
+            }
         }
 
         [HttpDelete("{id:int}")]
         public ActionResult Delete(int id)
         {
-            var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
-
-            if(produto is null)
+            try
             {
-                return NotFound("Produto não encontrado.");
+                var produto = _context.Produtos.FirstOrDefault(p => p.ProdutoId == id);
+
+                if (produto is null)
+                {
+                    return NotFound($"Produto com id = {id} não encontrado.");
+                }
+
+                _context.Produtos.Remove(produto);
+                _context.SaveChanges();
+
+                return Ok(produto);
             }
-
-            _context.Produtos.Remove(produto);
-            _context.SaveChanges();
-
-            return Ok(produto);
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Ocorreu um erro ao tratar sua solicitação.");
+            }
         }
     }
 }
--- ApiCatalogo/Program.cs
+++ ApiCatalogo/Program.cs
@@ -1,11 +1,14 @@
 using ApiCatalogo.Context;
 using Microsoft.EntityFrameworkCore;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(
+    options => options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles
+); // ajustar erro referência cíclica
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
